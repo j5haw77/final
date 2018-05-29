c359888438b215b8e7230a0841c18061b1500b00
Dylan Thornsberry <dythor@uw.edu>
2018-05-28 20:50:43 -0700

    changes

--- server.R
+++ server.R
@@ -6,13 +6,18 @@ library(DT)
 source("analysis.R")
 
 my_server <- function(input, output) {
+  
   output$data_table <- renderDT({
-    data %>% select(City, input$categories, Latitude, Longitude)
+    if (!is.null(input$categories)) {
+      data %>% select(City, input$categories, Latitude, Longitude)
+    } 
    })
   
-  output$plot_one <- renderPlot({
-    data %>% select(City, input$categories, Latitude, Longitude)
-    ggplot(data = data) 
+  output$pollute_plot <- renderPlot({
+    data %>% select(City, input$categories, Latitude, Longitude) %>% 
+    ggplot(aes(x='Pollution', y = data[input$select])) +
+      geom_point() +
+      theme_minimal()
       
   })
 }
