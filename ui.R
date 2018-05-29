59964f077b1d7293f292d32ef24357ee8714afc3
Sam Lee <kartd01033990101@qq.com>
2018-05-28 18:55:39 -0700

    Update .csv and edit ui.R

--- ui.R
+++ ui.R
@@ -3,6 +3,38 @@ library(dplyr)
 library(ggplot2)
 library(DT)
 
-ui <- fluidPage()
+source("analysis.R")
+
+ui <- fluidPage(
+  titlePanel("Washington Air & Weather"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(
+        "year",
+        "Years",
+        min = 2000,
+        max = 2016,
+        value = 2016
+      ),
+      selectInput(
+        "select",
+        label = "Select a category:",
+        choices = selections
+      ),
+    mainPanel(
+      tabsetPanel(
+        type = "tabs",
+        tabPanel("Table", 
+                 DT::dataTableOutput("data_table"),
+        tabPanel("Plot", 
+                 plotOutput("plot", click = "plot_click"), 
+                 tags$strong(textOutput("plot_selec"))
+                )
+        )
+      )
+    )
+  )
+)
+)
 
 shinyUI(ui)
