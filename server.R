9b9e84b8a3f21b288bcad6d3539f0b2641b71bab
Dylan Thornsberry <dythor@uw.edu>
2018-05-28 20:13:39 -0700

    changes

--- server.R
+++ server.R
@@ -7,13 +7,12 @@ source("analysis.R")
 
 my_server <- function(input, output) {
   output$data_table <- renderDT({
-
     data %>% select(City, input$categories, Latitude, Longitude)
    })
   
   output$plot_one <- renderPlot({
     data %>% select(City, input$categories, Latitude, Longitude)
-    ggplot(data = data) +
+    ggplot(data = data) 
       
   })
 }
